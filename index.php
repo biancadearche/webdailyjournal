eeeba311411c93050c1fd67ae6dbb9e8df573417
biancadearche <mufidafarah79@gmail.com>
2026-01-08 20:47:34 +0700

    initial commit latihan php 7

---
+++ index.php
@@ -0,0 +1,379 @@
+<?php
+//menyertakan code dari file koneksi
+include "koneksi.php";
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Daily Book</title>
+    <link rel="icon" href="img/logo.jpg"/>  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" 
+    crossorigin="anonymous"/>
+    <style>
+      body {
+        margin: 0;
+        font-family: Arial;
+        background-color: #ffffff; /* default light */
+        color: #111;
+        transition: 0.3s;
+        }
+
+        /* Tombol theme */
+        .btn-theme {
+            border: none;
+            padding: 8px 12px;
+            cursor: pointer;
+            border-radius: 6px;
+            font-size: 18px;
+        }
+
+        .dark-mode {
+            background-color: #222;
+            color: white;
+        }
+
+        .light-mode {
+            background-color: #a7a5a3;
+            color: #111;
+        }
+
+        /* Mode gelap */
+        body.dark {
+            background-color: #1a1a1a;
+            color: #f5f5f5;
+        }
+
+        .accordion-button:not(.collapsed) {
+        background-color: gray;
+        color: white;
+      }
+  </style>      
+
+  <body>
+<!-- nav begin -->
+  <nav class="navbar navbar-expand-lg bg-secondary-subtle sticky-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">Sora Cafe</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-dark">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#article">Article</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#gallery">Gallery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#schedule">Schedule</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about me">About Me</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login.php" target="_blank">Login</a>
+        </li>
+      </ul>
+
+    <div class="navbar">
+        <button class="btn-theme dark-mode" id="btnDark"><i class="bi bi-moon-stars-fill"></i></button>
+        <button class="btn-theme light-mode" id="btnLight"><i class="bi bi-sun"></i></button>
+    </div>
+
+    </div>
+  </div>
+</nav>
+<!-- nav end -->
+
+<!-- hero begin -->
+  <section id="hero" class="text-center p-5 text-sm-start">
+    <div class="p-3 mb-2 bg-secondary-subtle text-secondary-emphasis">
+      <div class="container">
+        <div class="d-sm-flex flex-sm-row-reverse align-items-center">
+          <img src="img/banner.jpg" class="img-fluid" width="300"/>
+          <div>
+            <h1 class="fw-bold display-4">Create Memories, Save Memories, Everyday</h1>
+            <h4 class="lead display-6">Mencatat semua kegiatan sehari-hari yang ada tanpa terkecuali</h4>
+            <span id="tanggal"></span>
+            <span id="jam"></span>
+          </div>
+        </div>
+      </div>
+    </div>  
+  </section>
+<!-- hero end -->
+
+<!-- article begin -->
+  <section id="article" class="text-center p-5">
+    <div class="container">
+      <h1 class="fw-bold display-4 pb-3">article</h1>
+    <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+  <?php
+  $sql = "SELECT * FROM article ORDER BY tanggal DESC";
+  $hasil = $conn->query($sql); 
+
+  while($row = $hasil->fetch_assoc()){
+  ?>
+
+<!--col begin-->
+  <div class="col">
+    <div class="card h-100">
+      <img src="img/<?= $row["gambar"]?>" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title"><?= $row["judul"]?></h5>
+        <p class="card-text"><?= $row["isi"]?></p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary"><?= $row["tanggal"]?></small>
+      </div>
+    </div>
+  </div>
+<!--col ends-->
+  <?php
+  }
+  ?>
+
+  </div>
+  </div>
+  </section>
+<!-- article end -->
+ 
+<!-- gallery begin -->
+  <section id="gallery" class="text-center p-5" >
+    <div class="p-3 mb-2 bg-secondary-subtle text-secondary-emphasis">
+    <div class="container">
+        <h1 class="fw-bold display-4 pb-3">gallery</h1>
+        
+    <div id="carouselExample" class="carousel slide">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/1.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="img/2.jpg" class="d-block w-100" alt="...">
+          </div>
+        <div class="carousel-item">
+          <img src="img/3.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="img/4.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="img/5.jpg" class="d-block w-100" alt="...">
+        </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    </div>
+    </div>
+  </section>
+<!-- gallery end -->
+
+<!--Schedule begin-->
+ <section id="schedule" class="text-center p-5">
+      <h1 class="fw-bold display-4 pb-3">Schedule</h1>
+      <div
+        class="row row-cols-2 row-cols-sm-3 row-cols-lg-4 g-4 justify-content-center">
+        <div class="col">
+          <div class="p-4 border rounded shadow-sm h-100">
+            <i class="bi bi-book text-dark-emphasis fs-1"></i>
+            <h5 class="mt-3">Membaca</h5>
+            <p>Menambah wawasan setiap pagi sebelum beraktivitas.</p>
+          </div>
+        </div>
+        <div class="col">
+          <div class="p-4 border rounded shadow-sm h-100">
+            <i class="bi bi-laptop text-dark-emphasis fs-1"></i>
+            <h5 class="mt-3">Menulis</h5>
+            <p>Mencatat setiap pengalaman harian di jurnal pribadi.</p>
+          </div>
+        </div>
+        <div class="col">
+          <div class="p-4 border rounded shadow-sm h-100">
+            <i class="bi bi-people text-dark-emphasis fs-1"></i>
+            <h5 class="mt-3">Diskusi</h5>
+            <p>Bertukar ide dengan teman dalam kelompok belajar.</p>
+          </div>
+        </div>
+        <div class="col">
+          <div class="p-4 border rounded shadow-sm h-100">
+            <i class="bi bi-bicycle text-dark-emphasis fs-1"></i>
+            <h5 class="mt-3">Olahraga</h5>
+            <p>Menjaga kesehatan dengan bersepeda sore hari.</p>
+          </div>
+        </div>
+        <div class="col">
+          <div class="p-4 border rounded shadow-sm h-100">
+            <i class="bi bi-film text-dark-emphasis fs-1"></i>
+            <h5 class="mt-3">Movie</h5>
+            <p>Menonton film yang bagus di bioskop.</p>
+          </div>
+        </div>
+        <div class="col">
+          <div class="p-4 border rounded shadow-sm h-100">
+            <i class="bi bi-bag text-dark-emphasis fs-1"></i>
+            <h5 class="mt-3">Belanja</h5>
+            <p>Membeli kebutuhan bulanan di supermarket.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+<!--Scedule end-->
+
+<!--aboutme begin-->
+<section id="about me" class="text-center p-5" >
+    <div class="p-3 mb-2 bg-secondary-subtle text-secondary-emphasis">
+    <div class="container">
+        <h1 class="fw-bold display-4 pb-3">About Me</h1>
+
+<div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Universitas Dian Nuswantoro Semarang (2024-Now)
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the first item’s accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. 
+        These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. 
+        It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        SMA Negri 1 Semarang (2024-2021)
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the second item’s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. 
+        These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. 
+        It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        SMP Negri 2 Semarang (2021-2018)
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the third item’s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. 
+        These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. 
+        It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</section>
+<!--aboutme end-->
+
+<!-- footer begin -->
+  <footer class="text-center p-5">
+    <div>
+      <a href="https://www.instagram.com/_faether/"><i class="bi bi-instagram h2 p-2 text-secondary"></i></a>
+      <a href="https://twitter.com/udinusofficial"><i class="bi bi-twitter h2 p-2 text-secondary"></i></a>
+      <a href="https://wa.me/+6288216170191"><i class="bi bi-whatsapp h2 p-2 text-secondary"></i></a>
+    </div><br>
+    <div>
+      Farah Kunia Mufida &copy; 2025
+    </div>
+  </footer>
+<!-- footer end -->
+
+<!-- Tombol Back to Top -->
+  <button
+    id="backToTop"
+    class="btn btn-secondary rounded-circle position-fixed bottom-0 end-0 m-3" d-none>
+      <i class="bi bi-arrow-up" title="Back to Top"></i>
+  </button>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" 
+    crossorigin="anonymous">
+  </script>
+
+<!--js begin-->
+  <script type="text/javascript">
+    const btnDark = document.getElementById("btnDark");
+    const btnLight = document.getElementById("btnLight");
+
+    btnDark.addEventListener("click", () => {
+      document.body.classList.add("dark");
+    });
+
+    btnLight.addEventListener("click", () => {
+      document.body.classList.remove("dark");
+    });
+  </script>
+
+  <script type="text/javascript">
+    function tampilwaktu(){
+    const waktu = new Date();
+
+    const tanggal = waktu.getDate();
+    const bulan = waktu.getMonth();
+    const tahun = waktu.getFullYear();
+    const jam = waktu.getHours();
+    const menit = waktu.getMinutes();
+    const detik = waktu.getSeconds();
+    
+    const arrBulan = ["1", "2", "3", "4","5","6","7","8","9","10","11","12"];
+
+    const tanggal_full = tanggal + "/" + arrBulan[bulan] + "/" + tahun;
+    const jam_full = jam + ":" + menit + ":" + detik;
+
+    document.getElementById("tanggal").innerHTML = tanggal_full;
+    document.getElementById("jam").innerHTML = jam_full;
+    }
+
+    setInterval(tampilwaktu, 1000);
+    </script>
+
+  <script type="text/javascript"> 
+    const backToTop = document.getElementById("backToTop");
+
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > 300) {
+        backToTop.classList.remove("d-none");
+        backToTop.classList.add("d-block");
+      } else {
+        backToTop.classList.remove("d-block");
+        backToTop.classList.add("d-none");
+      }
+  });
+  backToTop.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
+</script>
+
+<!--js end-->
+  </body>
+</html>
